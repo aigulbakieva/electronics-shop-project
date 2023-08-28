7e191f207901f14ba16e789d3970ae09cfc2f478
aigulbakieva <bakisha.a@mail.ru>
2023-08-28 22:23:19 +0100

    доб.тесты

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item_1= ("Смартфон", 1000, 20)
+item_2 = ("Ноутбоук", 20000, 5)
+
+def test_calculate_total_price():
+    assert calculate_total_price(item_2) == 100000
+    assert calculate_total_price(item_1) == 200000
+
+
+def test_apply_discount(item):
+    Item.pay_rate = 0.8
+    assert apply_discount(item_2) == 10000
