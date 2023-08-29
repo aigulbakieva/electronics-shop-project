49bc7e9344f16d75a73edccdb476b26946f9c1ab
aigulbakieva <bakisha.a@mail.ru>
2023-08-29 14:19:17 +0100

    исправление ошибок в тестах

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,13 +1,14 @@
 from src.item import Item
 
-item_1= ("Смартфон", 1000, 20)
-item_2 = ("Ноутбоук", 20000, 5)
+item1 = Item("Смартфон", 1000, 20)
+item2 = Item("Ноутбоук", 20000, 5)
 
+
+def test_init():
+    assert item1.price == 1000
+    assert item2.price == 20000
 def test_calculate_total_price():
-    assert calculate_total_price(item_2) == 100000
-    assert calculate_total_price(item_1) == 200000
+    assert item2.calculate_total_price() == 100000
+    assert item1.calculate_total_price() == 20000
 
 
-def test_apply_discount(item):
-    Item.pay_rate = 0.8
-    assert apply_discount(item_2) == 10000
